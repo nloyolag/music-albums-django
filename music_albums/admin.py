88c17466d41bc7b99346ca69e23ca6b997265c03
Noe Loyola <noeloyolag@gmail.com>
2016-01-23 10:13:53 -0600

    Created tutorial branch, tostart from scratch

--- music_albums/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Label, Album, Artist
-
-admin.site.register(Label)
-admin.site.register(Album)
-admin.site.register(Artist)
--- music_albums/urls.py
+++ music_albums/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from music_albums import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^dashboard/', views.Home.as_view(), name='home')
+    url(r'^dashboard/', views.Home.as_view(), name='home'),
+    url(r'^grappelli/', include('grappelli.urls')),
 ]
